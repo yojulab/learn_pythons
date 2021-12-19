85d67a2b6cbe85be59a220fc707955911796e3bf
Sanghun Oh <otter.oh@gmail.com>
2021-12-19 18:43:58 +0900

    add math function

---
+++ math_function.py
@@ -0,0 +1,17 @@
+
+import math
+# https://www.cuemath.com/geometry/radians-to-degrees/
+radians = math.pi   # 3.14159
+
+# Convert degrees to radians
+degrees = 90.0
+convert_radians = math.radians(degrees)
+print(convert_radians)
+radians_1 = math.radians(1)
+print(radians_1)
+
+# Convert radians to degrees
+convert_degrees = math.degrees(convert_radians)
+print(convert_degrees)
+
+pass
